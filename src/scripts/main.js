cd081c9ddf8b7fd19f9ea5fa477fb148d8db6240
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-09-30 17:32:34 +0200

    Center the spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+// Select elements
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+// Calculate centered position
+const centerX = (wall.clientWidth - spider.clientWidth) / 2;
+const centerY = (wall.clientHeight - spider.clientHeight) / 2;
+
+// Apply positioning
+spider.style.position = 'absolute';
+spider.style.left = centerX + 'px';
+spider.style.top = centerY + 'px';
